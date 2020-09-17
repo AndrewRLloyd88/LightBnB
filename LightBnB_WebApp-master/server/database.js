1928e0f12201eaf68dba9c91c3d80ded474ebf86
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-09-17 15:25:25 -0700

    all search functions are working folowing properties exercise

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -27,7 +27,7 @@ client.connect(() => {
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function(email) {
+const getUserWithEmail = function (email) {
   let userWithEmailQuery = "SELECT * ";
   userWithEmailQuery += "FROM users ";
   userWithEmailQuery += "WHERE email = $1;";
@@ -66,7 +66,7 @@ exports.getUserWithEmail = getUserWithEmail;
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function(id) {
+const getUserWithId = function (id) {
   let userWithIdQuery = "SELECT * ";
   userWithIdQuery += "FROM users ";
   userWithIdQuery += "WHERE id = $1;";
@@ -94,7 +94,7 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser = function(user) {
+const addUser = function (user) {
   let addUserQuery = " INSERT INTO users (name, email, password)";
   addUserQuery += " VALUES ($1, $2, $3)";
   addUserQuery += " RETURNING *;";
@@ -120,26 +120,26 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function(guest_id, limit = 10) {
-let getAllResQuery = 'SELECT reservations.*, properties.*, AVG(property_reviews.rating) AS average_rating'
-getAllResQuery += ' FROM properties'
-getAllResQuery += ' JOIN reservations ON properties.id = reservations.property_id'
-getAllResQuery += ' JOIN property_reviews ON properties.id = property_reviews.property_id'
-getAllResQuery += ' WHERE end_date < NOW()::date'
-getAllResQuery += ' AND reservations.guest_id = $1'
-getAllResQuery += ' GROUP BY reservations.id, properties.id'
-getAllResQuery += ' ORDER BY start_date'
-getAllResQuery += ' LIMIT $2;'
-console.log(getAllResQuery)
-return client.query(getAllResQuery, [guest_id, limit])
-.then(res => {
-  return res.rows;
-})
-.catch(err => {
-  return console.log('query error:', err);
-});
+const getAllReservations = function (guest_id, limit = 10) {
+  let getAllResQuery = 'SELECT reservations.*, properties.*, AVG(property_reviews.rating) AS average_rating'
+  getAllResQuery += ' FROM properties'
+  getAllResQuery += ' JOIN reservations ON properties.id = reservations.property_id'
+  getAllResQuery += ' JOIN property_reviews ON properties.id = property_reviews.property_id'
+  getAllResQuery += ' WHERE end_date < NOW()::date'
+  getAllResQuery += ' AND reservations.guest_id = $1'
+  getAllResQuery += ' GROUP BY reservations.id, properties.id'
+  getAllResQuery += ' ORDER BY start_date'
+  getAllResQuery += ' LIMIT $2;'
+  // console.log(getAllResQuery)
+  return client.query(getAllResQuery, [guest_id, limit])
+    .then(res => {
+      return res.rows;
+    })
+    .catch(err => {
+      return console.log('query error:', err);
+    });
 
-// SELECT reservations.*, properties.*, AVG(property_reviews.rating) AS average_rating FROM properties JOIN reservations ON properties.id = reservations.property_id JOIN property_reviews ON properties.id = property_reviews.property_id WHERE end_date < NOW()::date AND reservations.guest_id = $1 GROUP BY reservations.id, properties.id ORDER BY start_date LIMIT $2;
+  // SELECT reservations.*, properties.*, AVG(property_reviews.rating) AS average_rating FROM properties JOIN reservations ON properties.id = reservations.property_id JOIN property_reviews ON properties.id = property_reviews.property_id WHERE end_date < NOW()::date AND reservations.guest_id = $1 GROUP BY reservations.id, properties.id ORDER BY start_date LIMIT $2;
 
   // return getAllProperties(null, 2);
 };
@@ -153,13 +153,109 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function(options, limit = 10) {
-  let propQuery = 'Select * ';
-  propQuery += 'FROM properties';
-  propQuery += ' LIMIT $1';
+const getAllProperties = function (options, limit = 10) {
+
+  // 1. Setup an array to hold any parameters that may be available for the query.
+  const queryParams = [limit]
+  // 2. Start the query with all information that comes before the WHERE clause.
+  let propQuery = ['Select DISTINCT properties.* ']
+  propQuery.push('FROM properties');
+  console.log(propQuery)
+  // 3. Check if a city has been passed in as an option. Add the city to the params array and create a WHERE clause for the city.
+  //city only filled in
+  if (options.city && !options.minimum_price_per_night && !options.maximum_price_per_night && !options.minimum_rating) {
+    // 4 We can use the length of the array to dynamically get the $n placeholder number. Since this is the first parameter, it will be $1.
+    // 5. The % syntax for the LIKE clause must be part of the parameter, not the query.
+    queryParams.push("%" + options.city + "%")
+    console.log(queryParams)
+    propQuery.push(' WHERE properties.city LIKE $2')
+    //push to query params - and add the WHERE clause to our propQuery
+  }
+
+  //if city and rating is filled in but min/max price are empty
+  if(options.city && !options.minimum_price_per_night && !options.maximum_price_per_night && options.minimum_rating ){
+    //handle the rating first
+    propQuery.push(' JOIN property_reviews ')
+      propQuery.push(' ON properties.id = property_reviews.property_id ')
+      propQuery.push(' GROUP BY properties.id, property_reviews.id')
+      propQuery.push(' HAVING AVG(property_reviews.rating) >= $2 ')
+      queryParams.push(options.minimum_rating)
+    //handle the city afterwards
+      queryParams.push("%" + options.city + "%")
+      console.log(queryParams)
+      propQuery.push(' WHERE properties.city LIKE $3')
+  }
+
+  //if options.city and both min and max price are filled out
+  if (options.city && options.minimum_price_per_night && options.maximum_price_per_night && !options.minimum_rating) {
+      queryParams.push("%" + options.city + "%")
+      console.log(queryParams)
+      propQuery.push(' WHERE properties.city LIKE $2')
+
+      queryParams.push(options.minimum_price_per_night * 100, options.maximum_price_per_night * 100)
+      console.log(queryParams)
+      
+      // 6. Add any query that comes after the WHERE clause.
+      propQuery.push(' AND properties.cost_per_night > $3 AND properties.cost_per_night < $4 ')
+    }
+
+  //if price is only filled in
+  if (!options.city && options.minimum_price_per_night && options.maximum_price_per_night && !options.minimum_rating) {
+    queryParams.push(options.minimum_price_per_night * 100, options.maximum_price_per_night * 100)
+    console.log(queryParams)
+    
+    // 6. Add any query that comes after the WHERE clause.
+    propQuery.push(' AND properties.cost_per_night > $2 AND properties.cost_per_night < $3 ')
+  }
+
+    //if rating is only filled in
+    if (!options.city && !options.minimum_price_per_night && !options.maximum_price_per_night && options.minimum_rating) {
+      propQuery.push(' JOIN property_reviews ')
+      propQuery.push(' ON properties.id = property_reviews.property_id ')
+      propQuery.push(' GROUP BY properties.id, property_reviews.id')
+      propQuery.push(' HAVING AVG(property_reviews.rating) >= $2 ')
+      queryParams.push(options.minimum_rating)
+    }
+
+
+  //if everything is provided 
+  if (options.city && options.minimum_price_per_night && options.maximum_price_per_night && options.minimum_rating) {
+      propQuery.push(' JOIN property_reviews ')
+      propQuery.push(' ON properties.id = property_reviews.property_id ')
+      propQuery.push(' GROUP BY properties.id, property_reviews.id')
+      propQuery.push(' HAVING AVG(property_reviews.rating) >= $2 ')
+      queryParams.push(options.minimum_rating)
+      console.log(queryParams)
+      propQuery.push(' AND properties.city LIKE $3')
+      queryParams.push("%" + options.city + "%")
+      queryParams.push(options.minimum_price_per_night * 100, options.maximum_price_per_night * 100)
+      console.log(queryParams)
+      propQuery.push(' AND properties.cost_per_night > $4 AND properties.cost_per_night < $5 ')
+      console.log(queryParams)
+    }
+
+//     Select * FROM properties JOIN property_reviews  ON properties.id = property_reviews.property_id  GROUP BY properties.id, property_reviews.id HAVING AVG(property_reviews.rating) >= $2  AND properties.city LIKE $3 AND properties.cost_per_night > $4 AND properties.cost_per_night < $5  LIMIT $1
+// { error: invalid input syntax for type numeric: "%Vancouver%"
+
+  // Complete this query to allow the following 3 filters:
+
+  // if an owner_id is passed in, only return properties belonging to that owner.
+  // if a minimum_price_per_night and a maximum_price_per_night, only return properties within that price range.
+  // if a minimum_rating is passed in, only return properties with a rating equal to or higher than that.
+  // Remember that all of these may be passed in at the same time so they all need to work together. You will need to use AND for every filter after the first one. Also, none of these might be passed in, so the query still needs to work without a WHERE clause.
+
+  //limit is the last thing to be appended if still applicable?
+  propQuery.push(' LIMIT $1');
+  //at this point join all the query strings in propQuery array together to make our final query
+  propQuery = propQuery.join('')
+  // 7. Console log everything just to make sure we've done it right.
   console.log(propQuery);
-  return client.query(propQuery, [limit])
-    .then(res => res.rows).catch(err => console.log(err));
+  // 8. Run the query.
+  return client.query(propQuery, queryParams)
+    .then(res => {
+      // console.log(res.rows)
+      return res.rows
+    }).catch(err => console.log(err));
 };
 
 
@@ -171,7 +267,7 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function(property) {
+const addProperty = function (property) {
   const propertyId = Object.keys(properties).length + 1;
   property.id = propertyId;
   properties[propertyId] = property;
